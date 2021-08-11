7260f55c93921b5f47cab65a27ad725d121f856a
kbutyva <84053102+kbutyva@users.noreply.github.com>
2021-08-11 15:22:59 +0800

    second

---
+++ 2.2.2. spring_mvc/src/main/java/web/model/Car.java
@@ -0,0 +1,26 @@
+package web.model;
+
+
+public class Car {
+    private String model;
+    private int year;
+    private int power;
+
+    public Car() {
+    }
+
+    public Car(String model, int year, int power) {
+        this.model = model;
+        this.year = year;
+        this.power = power;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", year=" + year +
+                ", power=" + power +
+                '}';
+    }
+}
