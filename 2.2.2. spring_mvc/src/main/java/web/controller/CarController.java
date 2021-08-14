05df0cd5c1b9d9544272215727f6a635d5b3674a
kbutyva <84053102+kbutyva@users.noreply.github.com>
2021-08-14 17:26:49 +0800

    third

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -4,40 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
-
-import java.util.ArrayList;
-import java.util.List;
+import web.service.CarService;
 
 @Controller
 public class CarController {
 
-
     @GetMapping("/cars")
-    public String getCars(ModelMap model, @RequestParam(value = "count", required = false) String count){
+    public String getCars(ModelMap model, @RequestParam(value = "count", required = false) String count) {
 
-        if(count==null||Integer.parseInt(count)>=5) {
-            List<Car> messages = new ArrayList<>();
-            messages.add(new Car("lada", 1984, 90));
-            messages.add(new Car("ford", 2000, 250));
-            messages.add(new Car("opel", 2020, 160));
-            messages.add(new Car("toyota", 1960, 90));
-            messages.add(new Car("bmw", 1990, 120));
-            model.addAttribute("messages", messages);
-        } else if (Integer.parseInt(count) == 2){
-            List<Car> messages = new ArrayList<>();
-            messages.add(new Car("lada", 1984, 90));
-            messages.add(new Car("ford", 2000, 250));
-            model.addAttribute("messages", messages);
-        } else  if (Integer.parseInt(count) == 3){
-            List<Car> messages = new ArrayList<>();
-            messages.add(new Car("lada", 1984, 90));
-            messages.add(new Car("ford", 2000, 250));
-            messages.add(new Car("opel", 2020, 160));
-            model.addAttribute("messages", messages);
+        if (count == null || Integer.parseInt(count) >= 5) {
+            model.addAttribute("messages", new CarService().getFiveCars());
+        } else if (Integer.parseInt(count) == 2) {
+
+            model.addAttribute("messages", new CarService().getTwoCars());
+        } else if (Integer.parseInt(count) == 3) {
+
+            model.addAttribute("messages", new CarService().getThreeCars());
         }
         return "cars";
     }
-
 
 }
--- 2.2.2. spring_mvc/src/main/java/web/service/CarService.java
+++ 2.2.2. spring_mvc/src/main/java/web/service/CarService.java
@@ -1,5 +1,35 @@
 package web.service;
 
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarService {
 
+    public List getFiveCars() {
+
+        List<Car> messages = new ArrayList<>();
+        messages.add(new Car("lada", 1984, 90));
+        messages.add(new Car("ford", 2000, 250));
+        messages.add(new Car("opel", 2020, 160));
+        messages.add(new Car("toyota", 1960, 90));
+        messages.add(new Car("bmw", 1990, 120));
+        return messages;
+    }
+
+    public List getTwoCars(){
+        List<Car> messages = new ArrayList<>();
+        messages.add(new Car("lada", 1984, 90));
+        messages.add(new Car("ford", 2000, 250));
+        return messages;
+    }
+
+    public List getThreeCars(){
+        List<Car> messages = new ArrayList<>();
+        messages.add(new Car("lada", 1984, 90));
+        messages.add(new Car("ford", 2000, 250));
+        messages.add(new Car("opel", 2020, 160));
+        return messages;
+    }
 }
